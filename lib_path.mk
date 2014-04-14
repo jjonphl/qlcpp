72a356c9e74858058dabc0830fd0dcce8282fb51
Miguel Manese <jjonphl@gmail.com>
2014-04-14 14:01:48 +0800

    - centralized make paths

---
+++ lib_path.mk
@@ -0,0 +1,11 @@
+BOOST_1=../../boost_1_51_0
+QUANTLIB_H_1=../../QuantLib-1.4
+QUANTLIB_LIB_1=../../ql-1.4
+
+BOOST_2=../../../boost_1_51_0
+QUANTLIB_H_2=../../../QuantLib-1.4
+QUANTLIB_LIB_2=../../../ql-1.4
+
+BOOST=$(BOOST_$(DIR_LEVEL))
+QUANTLIB_H=$(QUANTLIB_H_$(DIR_LEVEL))
+QUANTLIB_LIB=$(QUANTLIB_LIB_$(DIR_LEVEL))
